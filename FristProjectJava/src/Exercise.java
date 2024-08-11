3403f2b949dd06d3054ff859c248065a0c94ee5a
Đào Thế Vinh <23702541.vinh@student.iuh.edu.vn>
2024-08-11 21:40:39 +0700

    Add files via upload

---
+++ FristProjectJava/src/Exercise.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Exercise {
+    public static void main (String[] args){
+
+        System.out.println("Xin chao moi nguoi minh la The Vinh");
+        System.out.println("Welcome to DHKTPT19B");
+
+    }
+}
